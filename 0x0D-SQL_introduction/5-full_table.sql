58062b9720f567de5b04b1c2babfa3dbc7b27d3b
Michael Getachew Abebe <mikygetyos@gmail.com>
2022-09-25 12:52:10 +0000

    Feat(add): added the code for solving the issues with the checker

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,5 @@
--- This script prints the description of th table first_table from the database hbtn_0c_0 in the MySQL server
+-- prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server
 -- The database name will be passed as an argument of the mysql command
--- You are not allowed to use the DESCRIBE or EXPLAIN statemens
+-- You are not allowed to use the DESCRIBE or EXPLAIN statements
 
-SHOW CREATE TABLE first_name;
+SHOW CREATE TABLE first_table;
